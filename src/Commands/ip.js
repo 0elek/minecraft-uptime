ef81682b279eaa32a62562956f2160e7ed38875f
elek <87550550+0elek@users.noreply.github.com>
2023-03-17 16:01:25 +0100

    Update ip.js

--- src/Commands/ip.js
+++ src/Commands/ip.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("ip")
-    .setDescription("Get ip address of the Minecraft servder"),
+    .setDescription("Get ip address of the Minecraft server"),
   execute: async (interaction, client, settings) => {
     return interaction.reply({ content: settings.server.address });
   },
