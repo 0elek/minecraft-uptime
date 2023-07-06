83bfa4b4fb6275818c0638a3f835ef22c3ea0fb9
0elek <87550550+0elek@users.noreply.github.com>
2023-07-07 01:57:20 +0200

    add random command

---
+++ src/Commands/random.ts
@@ -0,0 +1,17 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("random")
+    .setDescription("get a random number between ranges")
+    .addIntegerOption(option => option.setName('min')
+    .setDescription('The minimum number').setRequired(true))
+    .addIntegerOption(option => option.setName('max')
+    .setDescription('The maximum number').setRequired(true)),
+  execute: async (interaction: { reply: (arg0: { content: string; }) => any; }, client: { ws: { ping: any; }; }, settings: { server: { address: string; }; }) => {
+    let min = interaction.reply.arguments.getInteger('min');
+    let max = interaction.reply.arguments.getInteger('max');
+    let random = Math.floor(Math.random() * (max - min + 1)) + min;
+    return interaction.reply({ content: `Your random number is ${random}` });
+  },
+};
